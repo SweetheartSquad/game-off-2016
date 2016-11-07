6f57692d3fcd51cd2ef8705d603e931a0a09c5da
seleb <sean.s.leblanc@gmail.com>
2016-11-07 13:19:55 -0500

    card content placeholder text
    generateGame doesn't really do anything yet, all the placeholder content
    generation is in the drawGame loops

--- client/index.js
+++ client/index.js
@@ -45,31 +45,125 @@ function init(){
 
 		resource.img.src=toLoad[1];
 	}
+
+	character_w=size.x/4;
+	character_h=size.y/4;
+
+	file_w=size.x/10;
+	file_h=size.y/8;
+
+	ability_w=size.y/8;
+	ability_h=size.x/12;
+
+	board_w=size.x/2;
+	board_h=size.y/8*3;
 }
 
+function draw_character(character){
+	ctx.drawImage(images.character.img, 0, 0, character_w, character_h);
+
+	ctx.textAlign="left";
+	ctx.textBaseline="inherit";
+	ctx.fillStyle = "#FFFFFF";
+	
+	ctx.font = "bold "+DPI/4+"px Calibri";
+	for(var i = 0; i < character.implants.length; ++i){
+		ctx.fillText(character.implants[i], DPI/8, character_h/4*3+i*DPI/4);
+	}
+}
+
+function draw_file(file){
+	ctx.drawImage(images.file.img, 0, 0, file_w, file_h);
+
+	ctx.textAlign="center";
+	ctx.textBaseline="middle";
+	ctx.fillStyle = "#FFFFFF";
+
+	ctx.font = "bold "+DPI+"px Calibri";
+	ctx.fillText(file.points, file_w/2, file_h/2);
+
+	ctx.font = "bold "+DPI/4+"px Calibri";
+	ctx.fillText(suits[file.suit], file_w/2, file_h/8*7);
+}
+
+function draw_ability(ability){
+	ctx.drawImage(images.ability.img, 0, 0, ability_w, ability_h);
+
+	ctx.textAlign="left";
+	ctx.textBaseline="middle";
+	ctx.fillStyle = "#FFFFFF";
+
+	ctx.font = "bold "+DPI/8+"px Calibri";
+
+	var words=ability.description.split(" ");
+	var w=0;
+	var y=0;
+	var lines=[""];
+	while(words.length > 0){
+		w+=ctx.measureText(" "+words[0]).width;
+		if(w > ability_w/8*7){
+			y+=1;
+			w=ctx.measureText(" * "+words[0]).width;
+			lines[y]=" * ";
+		}
+		lines[y]+=words[0]+" ";
+		words.splice(0,1);
+	}
+
+	ctx.fillText("$ "+ability.category, DPI/8, ability_h/2+(-lines.length/2+0.5)*DPI/8);
+	for(var i = 0; i < lines.length; ++i){
+		ctx.fillText(lines[i], DPI/8, ability_h/2+(i-lines.length/2+1.5)*DPI/8);
+	}
+}
+
+
+
+function main(){
+	generateGame();
+	drawGame();
+	makePdf();
+}
+
+
+// procgen the varying elements of the game (TODO)
+function generateGame(){
+	suits=["A","B","C"];
+}
+
+// draw the game elements to the canvas
 function drawGame(){
 	canvas.width=size.x;
 	canvas.height=size.y;
 
-	var ctx = canvas.getContext("2d");
+	ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false; // nearest-neighbour vs. linear interpolation
 
 	// characters
 	var t=ctx.currentTransform;
 	for(var i = 0; i < 4; ++i){
-		ctx.drawImage(images.character.img, 0, 0, size.x/4, size.y/4);
-		ctx.translate(size.x/4,0);
+		draw_character({
+			// temp character
+			implants:[
+				"+1 for all A files",
+				"double all C files"
+			]
+		});
+		ctx.translate(character_w,0);
 	}
 	ctx.resetTransform();
 	
 	// files
-	ctx.translate(0,size.y/4);
+	ctx.translate(0,character_h);
 	for(var y = 0; y < 2; ++y){
 		for(var x = 0; x < 10; ++x){
-			ctx.drawImage(images.file.img, 0, 0, size.x/10, size.y/8);
-			ctx.translate(size.x/10,0);
+			draw_file({
+				// temp file
+				points:Math.floor(Math.random()*4),
+				suit:Math.floor(Math.random()*3)
+			});
+			ctx.translate(file_w,0);
 		}
-		ctx.translate(-size.x,size.y/8);
+		ctx.translate(-size.x,file_h);
 	}
 	ctx.resetTransform();
 	
@@ -79,26 +173,25 @@ function drawGame(){
 	for(var y = 0; y < 12; ++y){
 		for(var x = 0; x < 4; ++x){
 			if(x <= 0 || y >= 6){
-				ctx.drawImage(images.ability.img, 0, 0, size.y/8, size.x/12);
+				draw_ability({
+					// temp ability
+					category: "virus",
+					description: "/* hacker(s) with most files: discard 3 files */"
+				});
 			}
-			ctx.translate(size.y/8,0);
+			ctx.translate(ability_w,0);
 		}
-		ctx.translate(-size.y/2,size.x/12);
+		ctx.translate(-size.y/2,ability_h);
 	}
 	ctx.resetTransform();
 
 	// board
 	ctx.translate(size.x/2,size.y/8*5);
-	ctx.drawImage(images.board.img, 0, 0, size.x/2, size.y/8*3);
+	ctx.drawImage(images.board.img, 0, 0, board_w, board_h);
 	ctx.resetTransform();
 }
 
-function main(){
-	drawGame();
-	makePdf();
-}
-
-
+// canvas -> PDF process
 function makePdf(){
 
 	// convert canvas to image
