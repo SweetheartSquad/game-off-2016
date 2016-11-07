2a3ac8f2306c3229e287219b83b6861c4c0c015b
seleb <sean.s.leblanc@gmail.com>
2016-11-07 11:54:15 -0500

    board layout + placeholder background images
    -placeholder images are smallest integer size for the given ratio; can
    be scaled up but not down
    -some cards (just abilities atm) are rotated to fit the layout, but the
    images used for the cards are not (rotated with canvas code instead of
    rotating asset)

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
+var DPI=150;
 var size={
-	x:612,
-	y:792
+	x:8.5*DPI,
+	y:11*DPI
 };
 
 
@@ -14,11 +15,22 @@ images={
 	}
 };
 function init(){
+	// polyfill for resetTransform (not supported in IE)
+	CanvasRenderingContext2D.prototype.resetTransform=CanvasRenderingContext2D.resetTransform || function(){
+		this.setTransform(1, 0, 0, 1, 0, 0);
+	}
+
+
 	canvas = document.getElementById("canvas");
+	previewImg = document.getElementById("img-preview");
+	previewPdf = document.getElementById("pdf-preview");
 
 	load=[
 		["icon","assets/icon.png"],
-		["test","assets/img/test.png"]
+		["character","assets/img/character.png"],
+		["file","assets/img/file.png"],
+		["ability","assets/img/ability.png"],
+		["board","assets/img/board.png"]
 	];
 	
 	images.loading = load.length;
@@ -32,28 +44,53 @@ function init(){
 		resource.img.addEventListener("load", images.onImageLoaded.bind(images), false);
 
 		resource.img.src=toLoad[1];
-
 	}
 }
 
 function drawGame(){
 	canvas.width=size.x;
 	canvas.height=size.y;
+
 	var ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false; // nearest-neighbour vs. linear interpolation
 
-	// draw cloud
-	ctx.beginPath();
-	ctx.moveTo(170, 80);
-	ctx.rect(0,0,100,50);
-	ctx.closePath();
-	ctx.lineWidth = 5;
-	ctx.fillStyle = '#FF0000';
-	ctx.fill();
-	ctx.strokeStyle = '#0000FF';
-	ctx.stroke();
+	// characters
+	var t=ctx.currentTransform;
+	for(var i = 0; i < 4; ++i){
+		ctx.drawImage(images.character.img, 0, 0, size.x/4, size.y/4);
+		ctx.translate(size.x/4,0);
+	}
+	ctx.resetTransform();
+	
+	// files
+	ctx.translate(0,size.y/4);
+	for(var y = 0; y < 2; ++y){
+		for(var x = 0; x < 10; ++x){
+			ctx.drawImage(images.file.img, 0, 0, size.x/10, size.y/8);
+			ctx.translate(size.x/10,0);
+		}
+		ctx.translate(-size.x,size.y/8);
+	}
+	ctx.resetTransform();
+	
+	// abilities
+	ctx.translate(size.x,size.y/2);
+	ctx.rotate(Math.PI/2);
+	for(var y = 0; y < 12; ++y){
+		for(var x = 0; x < 4; ++x){
+			if(x <= 0 || y >= 6){
+				ctx.drawImage(images.ability.img, 0, 0, size.y/8, size.x/12);
+			}
+			ctx.translate(size.y/8,0);
+		}
+		ctx.translate(-size.y/2,size.x/12);
+	}
+	ctx.resetTransform();
 
-	ctx.drawImage(images.icon.img, 50,50);
-	ctx.drawImage(images.test.img, 150,50);
+	// board
+	ctx.translate(size.x/2,size.y/8*5);
+	ctx.drawImage(images.board.img, 0, 0, size.x/2, size.y/8*3);
+	ctx.resetTransform();
 }
 
 function main(){
@@ -68,25 +105,19 @@ function makePdf(){
 	var dataURL = canvas.toDataURL('image/png');
 
 	// update IMG preview in web page
-	document.getElementById("img-preview").setAttribute("src", dataURL);
+	previewImg.setAttribute("src", dataURL);
 
 
 	// copy image to PDF
-	var doc = new jsPDF({
-		orientation: 'landscape',
-		unit: 'px',
-		format: [size.x, size.y]
-	});
-	doc.addImage(dataURL, "PNG", 0, 0);
-	doc.setDisplayMode("fullpage", "twoleft", "UseThumbs");
-	doc.addPage();
+	var doc = new jsPDF('portrait','in','letter');
+	doc.addImage(dataURL, "PNG", 0, 0, 8.5, 11);
 	
 	// update PDF preview in web page
 	var blobUri = doc.output('bloburi');
-	document.getElementById("pdf-preview").setAttribute("src", blobUri);
+	previewPdf.setAttribute("src", blobUri);
 	
 	// download PDF
-	doc.save('test.pdf');
+	//doc.save('test.pdf');
 }
 
 
