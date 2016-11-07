4537bef928309ac2200a551ca675c8c4fd18bd04
seleb <sean.s.leblanc@gmail.com>
2016-11-07 08:37:10 -0500

    simple image loader

--- client/index.js
+++ client/index.js
@@ -4,25 +4,62 @@ var size={
 };
 
 
-var canvas = document.getElementById("canvas");
-canvas.width=size.x;
-canvas.height=size.y;
-var ctx = canvas.getContext("2d");
+images={
+	loading:0,
+	loaded:0,
+	onImageLoaded: function(){
+		if(++this.loaded == this.loading){
+			main();
+		}
+	}
+};
+function init(){
+	canvas = document.getElementById("canvas");
 
-// draw cloud
-ctx.beginPath();
-ctx.moveTo(170, 80);
-ctx.rect(0,0,100,50);
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.strokeStyle = '#0000FF';
-ctx.stroke();
+	load=[
+		["icon","assets/icon.png"],
+		["test","assets/img/test.png"]
+	];
+	
+	images.loading = load.length;
+	for(var i = 0; i < load.length; ++i){
+		var toLoad=load[i];
+		var resource={
+			img:new Image()
+		};
+		images[toLoad[0]] = resource;
+		resource.img.setAttribute('crossOrigin', 'anonymous');
+		resource.img.addEventListener("load", images.onImageLoaded.bind(images), false);
 
-makePdf();
+		resource.img.src=toLoad[1];
 
+	}
+}
 
+function drawGame(){
+	canvas.width=size.x;
+	canvas.height=size.y;
+	var ctx = canvas.getContext("2d");
+
+	// draw cloud
+	ctx.beginPath();
+	ctx.moveTo(170, 80);
+	ctx.rect(0,0,100,50);
+	ctx.closePath();
+	ctx.lineWidth = 5;
+	ctx.fillStyle = '#FF0000';
+	ctx.fill();
+	ctx.strokeStyle = '#0000FF';
+	ctx.stroke();
+
+	ctx.drawImage(images.icon.img, 50,50);
+	ctx.drawImage(images.test.img, 150,50);
+}
+
+function main(){
+	drawGame();
+	makePdf();
+}
 
 
 function makePdf(){
@@ -51,3 +88,8 @@ function makePdf(){
 	// download PDF
 	doc.save('test.pdf');
 }
+
+
+
+
+init();
